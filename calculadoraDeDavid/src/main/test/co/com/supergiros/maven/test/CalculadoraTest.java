5bcb9f679c95204943448ffe0fd2f08bbe13cb9d
dplaza <davidplaza_20@hotmail.com>
2019-06-10 13:49:15 -0500

    Se agrego la operación multiplicación

--- calculadoraDeDavid/src/main/test/co/com/supergiros/maven/test/CalculadoraTest.java
+++ calculadoraDeDavid/src/main/test/co/com/supergiros/maven/test/CalculadoraTest.java
@@ -6,22 +6,28 @@ import co.com.supergiros.demo.Calcular;
 
 public class CalculadoraTest {
 
-	Calcular app;
+	Calcular app = new Calcular();
 	double a = 5;
 	
 	 @Test
 	    public void suma()
 	    {
-	        app = new Calcular();
 	        double suma = app.suma(a,3);
 	        System.out.println("El resultado de la suma es "+suma);
 	    }
+	 
+	 @Test
+	    public void multiplicacion()
+	    {
+	        double multiplicacion = app.multiplicacion(a,3);
+	        System.out.println("El resultado de la multiplicación es "+"="+ multiplicacion);
+	    }
+	 
 	 
 
 	 @Test
 	    public void division()
 	    {
-	        app = new Calcular();
 	        double division = app.division(a,0);
 	        if(division==0) {
 	        System.out.println("El resultado de la division es "+division +" "+"ya que es una division por 0");
